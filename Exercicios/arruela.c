91319c39ec5faf7b3ffbefd8d747ada23936e741
Beatriz Paz Faria <estudo.bea@gmail.com>
2022-09-14 12:07:50 +0000

    aula07

--- Exercicios/arruela.c
+++ Exercicios/arruela.c
@@ -7,7 +7,7 @@ double calc_massa_arruela(double diametro_int, double diametro_ext, double espes
 
 int main(void)
 {
-    double d_int, d_ext, espessura, densidade;
+    double d_int, d_ext, espessura, densidade, ext_raio, int_raio;
     int num_arruela;
 
     printf("Entre com o diametro interno (cm): ");
@@ -25,28 +25,31 @@ int main(void)
     printf("Entre com o numero de arruelas: ");
     scanf("%d", &num_arruela);
 
-    printf("Massa total: %.2lf g\n", calc_massa_arruela(d_int, d_ext, espessura, densidade) * num_arruela);
+    int_raio = d_int / 2.0;
+    ext_raio = d_ext / 2.0;
+
+    printf("Massa total: %.2lf g\n", calc_massa_arruela(int_raio, ext_raio, espessura, densidade) * num_arruela);
 
     return 0;
 }
 
-double calc_area_circulo(double d_externo)
+double calc_area_circulo(double raio_externo)
 {
     double area_circ;
-    area_circ = M_PI * (d_externo * d_externo)/4.0;
+    area_circ = M_PI * (raio_externo * raio_externo);
     return area_circ;
 }
 
-double calc_area_anel(double d_ext, double d_int)
+double calc_area_anel(double raio_ext, double raio_int)
 {
     double area_anel;
-    area_anel = calc_area_circulo(d_ext) - M_PI * (d_int * d_int)/4.0;
+    area_anel = calc_area_circulo(raio_ext) - M_PI * (raio_int * raio_int);
     return area_anel;
 }
 
-double calc_massa_arruela(double diametro_int, double diametro_ext, double espessura, double rho)
+double calc_massa_arruela(double raio_int, double raio_ext, double espessura, double rho)
 {
     double massa;
-    massa = rho * (calc_area_anel(diametro_ext, diametro_int)) * espessura;
+    massa = rho * (calc_area_anel(raio_ext, raio_int)) * espessura;
     return massa;
 }
