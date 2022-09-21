d5d2a20d9e76da88edf8c59b02c53f631b01cc0d
Beatriz Paz Faria <estudo.bea@gmail.com>
2022-09-21 00:39:55 +0000

    exercico

--- Exercicios/formbhaskara.c
+++ Exercicios/formbhaskara.c
@@ -6,7 +6,7 @@ int main(void)
     float a, b, c;
     float delta, x1, x2, i1;
 
-    printf("Fórmula de Bhaskara\n\n");
+    printf("\nFórmula de Bhaskara\n\n");
 
     printf("Entre com os coeficientes A, B e C: ");
     scanf("%f%f%f", &a, &b, &c);
@@ -14,16 +14,24 @@ int main(void)
     delta = (b * b) - (4 * a * c);
 
     if (delta > 0) {
-    
         x1 = (-b + sqrt(delta)) / (2 * a);
         x2 = (-b - sqrt(delta)) / (2 * a);
         printf("Duas raizes reais: %f  %f\n", x1, x2);
-
-    } else if (delta < 0) {
+    if (delta == 0.0) {
+        printf("Uma raiz real: %f ", ((-b + sqrt(delta)) / (2 * a)));
+        } else if (delta < 0) {
+            printf("Duas raizes imaginarias: %fi %fi", (-b + sqrt(delta)) / (2 * a), (-b - sqrt(delta)) / (2 * a));
+        }else
+        {
+            printf("Duas raizes complexas:");
+        }
+    
+    /*} else if (delta < 0) {
         i1 = (-b + sqrt(-delta)) / (2 * a);
     }else{
         printf("Impossivel\n");
-    }
+    }*/
 
     return 0;
+    }
 }
