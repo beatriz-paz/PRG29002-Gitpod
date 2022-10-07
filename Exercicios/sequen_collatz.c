28b32a46c38719f0114e8ca9432ae2361b1ba49a
Beatriz Paz Faria <estudo.bea@gmail.com>
2022-10-07 01:29:24 +0000

    exercicio

--- Exercicios/sequen_collatz.c
+++ Exercicios/sequen_collatz.c
@@ -2,6 +2,21 @@
 
 int main(void)
 {
-    
+    int num, i;
+
+    printf("Entre com o termo inicial: ");
+    scanf("%d", &num);
+
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+            printf("%2d, ", num);
+        } else {
+            num = (3 * num) + 1;
+            printf("%2d, ", num);
+        }
+    }
+    printf("\n");
+
     return 0;
 }
