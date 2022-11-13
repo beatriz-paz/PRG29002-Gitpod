8b0fa3d9e08aad7f0db468bd63e16de4662395f4
Beatriz Paz Faria <estudo.bea@gmail.com>
2022-11-13 21:15:04 +0000

    exercicio

--- Aula16/strrepet_v2.c
+++ Aula16/strrepet_v2.c
@@ -4,9 +4,16 @@ void string_rep(char *nome, int n, char *nome_rep);
 
 int main(void)
 {
-    char nome[10] = "Bea";
+    char nome[10];
     char nome_rep[100];
-    string_rep(nome, 3, nome_rep);
+    int n;
+
+    printf("Entre com uma palavra: ");
+    scanf("%s", nome);
+    printf("Entre com o número de repetições: ");
+    scanf("%d", &n);
+
+    string_rep(nome, n, nome_rep);
     printf("%s\n", nome_rep);
 
     return 0;
@@ -14,19 +21,21 @@ int main(void)
 
 void string_rep(char *nome, int n, char *nome_rep)
 {
-    int i, j, rep;
+    int i = 0, j, rep = 1, ult_pos;
     i = 0;
+    rep = 1;
 
     while (nome[i] != 0) {
         nome_rep[i] = nome[i];
         i++;
     }
 
+    ult_pos = i - 1;
     j = i;
     i = 0;
 
     while (rep < n) {
-        for (i = 0; i < j - i; i++) {
+        for (i = 0; i <= ult_pos; i++) {
             nome_rep[j] = nome[i];
             j++;
         }
@@ -34,6 +43,4 @@ void string_rep(char *nome, int n, char *nome_rep)
         nome_rep[j] = 0;
         rep++;
     }
-    
-
 }
