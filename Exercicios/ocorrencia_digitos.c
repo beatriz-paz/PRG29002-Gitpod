158869e215a87549eb3fd130bb2353db2f0ca65a
Beatriz Paz Faria <estudo.bea@gmail.com>
2022-11-14 12:55:37 +0000

    exercicio

---
+++ Exercicios/ocorrencia_digitos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Entre com um inteiro: ");
+    scanf("%d", &n);
+
+    printf("Digito: 0 1 2 3 4 5 6 7 8 9 \n");
+    
+    printf("Ocorrencias: ");
+
+    return 0;
+}
