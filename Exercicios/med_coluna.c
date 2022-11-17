d615bc76514b4a859f7f0458a037d872a39ecbf5
Beatriz Paz Faria <estudo.bea@gmail.com>
2022-11-17 12:21:00 +0000

    exercicio media coluna feito

--- Exercicios/med_coluna.c
+++ Exercicios/med_coluna.c
@@ -3,18 +3,28 @@
 int main(void)
 {
     int i, j, l, c;
-    int matriz[100][100];
+    double soma, media;
+    double matriz[100][100];
     printf("Entre com as dimensoes da matriz: ");
     scanf("%d%d", &l, &c);
+    printf("Entre com os elementos da matriz: \n");
 
-    for (i = 0; i <= l; i++) {
-        for (j = 0; j <= c; j++) {
-             matriz[i][j];
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%lg", &matriz[i][j]);
         }
     }
 
-    printf("Entre com os elementos da matriz: ");
-    scanf("const char *restrict __format, ...");
+    printf("Entre com o indice da coluna: ");
+    scanf("%d", &c);
+
+    for (i = 0; i < l; i++) {
+        for (j = c; j == c; j++) {
+            soma += matriz[i][j];
+        }
+    }
+
+    printf("Media da coluna: %lg\n", media = soma / i);
 
     return 0;
 }
