fba50cb7731bb4969b3d6a8cc2e3473de6fde928
Beatriz Paz Faria <estudo.bea@gmail.com>
2023-02-08 01:44:55 +0000

    tarefa01

--- Aula01/Tarefa 01/calculador de nota.c
+++ Aula01/Tarefa 01/calculador de nota.c
@@ -2,21 +2,25 @@
 
 int main(void)
 {
-    char nota1[3];
-    char nota2[3];
-    char nota3[3];
+    int n1;
+    int n2;
+    int n3;
 
-    printf("Entre com o conceito 1: ");
-    scanf("%s", nota1);
+    printf("Entre com o conceito 1 (E-10, P-8, S-6 e I-1): ");
+    scanf("%d", &n1);
 
-    printf("Entre com o conceito 2: ");
-    scanf("%s", nota2);
+    printf("Entre com o conceito 2 (E-10, P-8, S-6 e I-1): ");
+    scanf("%d", &n2);
 
-    printf("Entre com o conceito 3: ");
-    scanf("%s", nota3);
+    printf("Entre com o conceito 3 (E-10, P-8, S-6 e I-1): ");
+    scanf("%d", &n3);
 
-    if (nota1 == nota2) {
-    statements
+    if (n1 + n2 + n3 > 28) {
+        printf("Conceito final: Excelente\n");
+    } else if (n1 + n2 + n3 > 22) {
+        printf("Conceito final: Proeficiente\n");
+    } else if (n1 + n2 + n3 < 22) {
+        printf("Conceito final: Suficiente\n");
     }
 
     return 0;
