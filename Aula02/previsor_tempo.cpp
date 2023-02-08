f2b8503a4cb126c174288bac451851d6c413e7a4
Beatriz Paz Faria <estudo.bea@gmail.com>
2023-02-08 14:26:29 +0000

    aula02

---
+++ Aula02/previsor_tempo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string bar, temp;
+
+    cout << "Barometro: ";
+    cin >> bar;
+    cout << "Termometro: ";
+    cin >> temp;
+
+    return 0;
+}
