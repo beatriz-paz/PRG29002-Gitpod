b17d89e6faa6b665e73333ee4fb2300dd7942584
Beatriz Paz Faria <75676327+beatriz-paz@users.noreply.github.com>
2023-03-12 11:08:05 -0300

    nova versao

--- Aula04/procura_palavra.cpp
+++ Aula04/procura_palavra.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <errno.h>
-
+#include <fstream>
 
 using namespace std;
+//O nome do arquivo a ser lido é informado no primeiro argumento de linha de comando
+//e a sequência a ser procurada está no segundo argumento de linha de comando.
+int main(int argc, char* argv[]){
 
-int main(int argc, char * argv[]){
-    
-    if(argc < 3){
-        cout << "Uso: " << argv[0] << " arquivo sequencia" << endl;
+    string linha, caracter = argv[2];
+    ifstream arq(argv[1]); //cria, e já abre um arquivo chamado ARQ para leitura
+
+     if(argc < 3){
         return 1;
     }
     
-    ifstream arq(argv[1]); //cria um arquivo
-    
-    if(! arq.is_open()){
-        perror("ao abrir");
+    // verifica se o arquivo foi aberto corretamente
+    if (! arq.is_open()) {
+        perror("Ao abrir /etc/hosts");
         return errno;
     }
-    
-    string sequencia = argv[2];
-    string linha;
-    
-    while(getline(arq, linha)){
-        if(linha.find(sequencia) != string::npos){
+
+    // lê cada linha do arquivo, e imprime a linha que possui uma certa sequencia de caracteres
+
+    while (getline(arq, linha)) {
+        if (linha.rfind(caracter) != string::npos){ //se encontrar a sequencia de caracter na linha, imprime ela
             cout << linha << endl;
         }
-        
     }
-    
-    
+
     return 0;
 }
