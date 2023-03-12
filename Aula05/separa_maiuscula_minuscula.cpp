76b914316dd13b1a50261a9599fc10b93fccd5f5
Beatriz Paz Faria <75676327+beatriz-paz@users.noreply.github.com>
2023-03-12 14:58:19 -0300

    Update separa_maiuscula_minuscula.cpp

--- Aula05/separa_maiuscula_minuscula.cpp
+++ Aula05/separa_maiuscula_minuscula.cpp
@@ -1,1 +1,47 @@
-#include 
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <error.h>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    queue<string>maiusculas;
+    queue<string>minusculas;
+    string linha, palavra;
+
+    ifstream arq(argv[1]);
+
+    if (! arq.is_open()){
+        perror("Ao abrir ");
+        return errno;
+    }
+
+    while (getline(arq, linha)){
+        istringstream frase(linha);
+        while (frase >> palavra){
+            int i = 0;
+            if (isupper(palavra[i])){ // isupper = verifica se a primeira letra é maiuscula
+                maiusculas.push(palavra);
+            } else{
+                minusculas.push(palavra);
+            }
+        }
+    }
+
+    while (! maiusculas.empty()) {
+        cout << maiusculas.front() << ' ';
+        maiusculas.pop();
+    }
+
+    cout << endl;
+
+    while (! minusculas.empty()) {
+        cout << minusculas.front() << ' ';
+        maiusculas.pop();
+    }
+
+    return 0;
+}
